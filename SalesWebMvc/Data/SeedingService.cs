e2b87ad9d89e8670006a2a885d9d53b09be79b39
“Renato <renk@MacBook-Air-de-Renato.local>
2018-09-17 12:50:53 -0300

    Seeding Service

---
+++ SalesWebMvc/Data/SeedingService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SalesWebMvc.Models;
+
+namespace SalesWebMvc.Data
+{
+    public class SeedingService
+    {
+        private SalesWebMvcContext _context;
+
+        public SeedingService(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        public void Seed()
+        {
+            if (_context.Department.Any() ||
+                _context.Seller.Any() ||
+                _context.SalesRecord.Any())
+            {
+                return;
+            }
+
+            Department d1 = new Department(1, "Computers");
+            Department d2 = new Department(2, "Eletronics");
+
+            Seller s1 = new Seller(1, "Bob Brown", "bob@gmail.com", new DateTime(1998, 4, 21), 1000.0, d1);
+
+            _context.Department.AddRange(d1, d2);
+            _context.Seller.AddRange(s1);
+
+            _context.SaveChanges();
+        }
+    }
+}
